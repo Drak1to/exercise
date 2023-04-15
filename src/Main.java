cce223990752df221961a08d59e647c1653bc19f
Drakonych <vitaliynepiyvoda@gmail.com>
2023-04-15 12:47:37 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,86 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {6, 6, 9, 3,};
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+
+        }
+        System.out.println(sum / 4);
+        {
+
+        }
+        int i = 123456789;
+        System.out.println(i);
+        {
+
+        }
+        int[] array1 = {6, 6, 9, 3,};
+        int sum1 = 1;
+        for (int a = 0; a < array1.length; a++) {
+            sum1 += array1[a];
+        }
+        System.out.println(sum1);
+        {
+
+        }
+        int q = 123456789;
+        System.out.println(q);
+        {
+
+        }
+        int[] array9 = {6, 3, 4, 9};
+        for (int d = 0; d < array9.length; d++) {
+            if (array9[d] % 2 == 0) {
+                System.out.println(array9[d]);
+            } else if (array9[d] % 2 == 1) {
+                System.out.println(0);
+            }
+        }
+        {
+            int p = 123456789;
+            System.out.println(p);
+            {
+            }
+            String[] string = {"Java is a programming language.", "Python is also a popular language.", "Java is used for web development.", "C++ is used for game development.", "Java is used for Android development.", "JavaScript is used for web development.", "Java is widely used in industry.", "Java has a large developer community.", "Java is platform-independent.", "Java was originally developed by Sun Microsystems.", "Java is a general-purpose language.", "Java is easy to learn."};
+            for (int g = 0; g < string.length; g++) {
+                if (string[g].contains("Java")) {
+                    System.out.println(string[g]);
+                }
+            }
+
+        }
+        int c = 123456789;
+        System.out.println(c);
+        {
+            int[] array8 = {6, -6, 9, -3,};
+            for (int m = 0; m < array8.length; m++) {
+                if (array8[m] < 1) {
+                    System.out.println(array8[m]);
+                }
+            }
+            int l = 123456789;
+            System.out.println(l);
+            {
+                int[] array3 = {6, 7, 6, 1, 4, 9, 5, 4, 2, 1};
+                for (int e = 0; e < array3.length; e++) {
+                    if (array3[e] % 2 == 1) {
+                        System.out.println(array3[e]);
+
+                    }
+
+
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
